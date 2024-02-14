9116c7c015febd933c8cd15e5835e7fdce4b8337
Jhon <jhon@lepidus.com.br>
2024-02-14 17:53:03 -0400

    Adds integration test to author listing in authors page
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#751
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

--- cypress/tests/Test1_authorsPage.cy.js
+++ cypress/tests/Test1_authorsPage.cy.js
@@ -1,6 +1,14 @@
 describe('Validatiations in the Authors page', function () {
+    const expectedAuthorsCount = 4;
+    const expectedAuthorsNames = ["Karbasizaed, Vajiheh", "Mansour, Amina", "Mwandenga, Alan", "Riouf, Nicolas"];
+    
     it('Access the authors page', function () {
         cy.visit('publicknowledge/authors');
         cy.get('h1').should('contain', 'Authors');
+        cy.get('#authorsList').children().should('have.length', expectedAuthorsCount);
+
+        for (authorName in expectedAuthorsNames) {
+            cy.contains('a', authorName);
+        }
     });
 });
