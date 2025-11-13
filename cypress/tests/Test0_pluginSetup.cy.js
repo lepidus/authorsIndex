1b39acbf98c27bb0019595a480fdb888c907f7be
carlos <carlos@lepidus.com.br>
2025-11-13 10:46:26 -0400

    Update tests
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#1076
    
    Signed-off-by: carlos <carlos@lepidus.com.br>

--- cypress/tests/Test0_pluginSetup.cy.js
+++ cypress/tests/Test0_pluginSetup.cy.js
@@ -2,10 +2,11 @@ describe('Plugin setup of "Authors Index plugin"', function () {
     it('Enables "Authors Index plugin"', function () {
 		cy.login('dbarnes', null, 'publicknowledge');
 
-		cy.contains('a', 'Website').click();
+        cy.get('nav').contains('Settings').click();
+        cy.get('nav').contains('Website').click({ force: true });
 
 		cy.waitJQuery();
-		cy.get('#plugins-button').click();
+        cy.get('button[id="plugins-button"]').click();
 
 		cy.get('input[id^=select-cell-authorsindexplugin]').check();
 		cy.get('input[id^=select-cell-authorsindexplugin]').should('be.checked');
